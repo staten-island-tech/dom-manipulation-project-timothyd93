969bb3edd84d14ef8ad97120fb4085502a96114e
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2022-11-04 13:58:53 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
-const button = document.getElementById("btn");
-console.log(button);
+const DOMSelectors = {
+  button: document.getElementById("btn"),
+  text: document.querySelector("#text"),
+  box: document.getElementById("big-black-box"),
+  points: document.querySelectorAll(".point"),
+};
+console.log(DOMSelectors);
