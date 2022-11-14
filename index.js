bc48ef10d2498fd6327589ea4fe62504faf0a102
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2022-11-14 18:15:28 -0500

    "remove" button

--- index.js
+++ index.js
@@ -1,33 +1,29 @@
 const DOMSelectors = {
   box: document.getElementById("container-box"),
-  button: document.getElementById("btn"),
+  result: document.getElementById("result"),
+  erase: document.getElementById("erase"),
+  remove: document.getElementById("remove"),
+  print: document.querySelector(`#print`),
   name: document.querySelector(`#name`),
   genre: document.querySelector(`#genre`),
   book: document.querySelector(`#book`),
   author: document.querySelector(`#author`),
 };
 
-// DOMSelectors.button.addEventListener("click", function () {
-//   let input = DOMSelectors.input.value;
-//   DOMSelectors.box.insertAdjacentHTML("beforeend", `<h1>${input}<h1>`);
-//   DOMSelectors.input.value = "";
-// });
-// DOMSelectors.button.addEventListener("click", function () {
-//   let input2 = DOMSelectors.input.value;
-//   DOMSelectors.box.insertAdjacentHTML("beforeend", `<p>${input2}</p>`);
-//   DOMSelectors.input.value = "";
-// });
-DOMSelectors.button.addEventListener("click", function () {
+DOMSelectors.result.addEventListener("click", function () {
   let name = DOMSelectors.name.value;
   let book = DOMSelectors.book.value;
   let genre = DOMSelectors.genre.value;
   let author = DOMSelectors.author.value;
   DOMSelectors.box.insertAdjacentHTML(
     "beforeend",
-    `<p>My name is ${name}. My favorite book is ${book}. My favorite book genre is ${genre}. My favorite author is ${author}</p>`
+    `<p id="print">Your name is ${name}. Your favorite book is ${book}. Your favorite genre is ${genre}. Your favorite author is ${author}</p>`
   );
   DOMSelectors.name.value = "";
   DOMSelectors.book.value = "";
   DOMSelectors.genre.value = "";
   DOMSelectors.author.value = "";
+});
+DOMSelectors.remove.addEventListener("click", function () {
+  document.getElementById("print").remove();
 });
