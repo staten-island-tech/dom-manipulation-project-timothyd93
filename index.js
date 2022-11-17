ea29e00edb45f7d43c5024642c46a7a27c047525
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2022-11-17 14:01:33 -0500

    erase button

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
+// const DOMSelectors = {
+//   box: document.getElementById("container-box"),
+//   result: document.getElementById("result"),
+//   erase: document.getElementById("erase"),
+//   name: document.querySelector(`#name`),
+//   book: document.querySelector(`#book`),
+//   author: document.querySelector(`#author`),
+//   genre: document.querySelector(`#genre`),
+// };
+
+// DOMSelectors.result.addEventListener("click", function () {
+//   const info = {
+//     name: DOMSelectors.name.value,
+//     book: DOMSelectors.book.value,
+//     author: DOMSelectors.author.value,
+//     genre: DOMSelectors.genre.value,
+//   };
+//   DOMSelectors.box.insertAdjacentHTML(
+//     "beforeend",
+//     `<h1 id="yourname">Hello ${info.name}</h1> <p id ="yourinfo">Your favorite book is ${info.book}. Your favorite author is ${info.author}. Your favorite genre is ${info.genre}</p>`
+//   );
+//   DOMSelectors.name.value = "";
+//   DOMSelectors.book.value = "";
+//   DOMSelectors.author.value = "";
+//   DOMSelectors.genre.value = "";
+//   // DOMSelectors.erase.addEventListener("click", function () {
+//   //   document.getElementById("yourname").remove();
+//   //   document.getElementById("yourinfo").remove();
+//   // });
+// });
+// DOMSelectors.erase.addEventListener("click", function () {
+//   document.getElementById("yourname").remove();
+//   document.getElementById("yourinfo").remove();
+// });
 const DOMSelectors = {
   box: document.getElementById("container-box"),
   result: document.getElementById("result"),
-  erase: document.getElementById("erase"),
   name: document.querySelector(`#name`),
   book: document.querySelector(`#book`),
   author: document.querySelector(`#author`),
   genre: document.querySelector(`#genre`),
 };
-
+let count = 0;
 DOMSelectors.result.addEventListener("click", function () {
   const info = {
     name: DOMSelectors.name.value,
@@ -15,20 +48,23 @@ DOMSelectors.result.addEventListener("click", function () {
     author: DOMSelectors.author.value,
     genre: DOMSelectors.genre.value,
   };
-
+  count += 1;
   DOMSelectors.box.insertAdjacentHTML(
     "beforeend",
-    `<h1 id="yourname">Hello ${info.name}</h1> <p id ="yourinfo">Your favorite book is ${info.book}. Your favorite author is ${info.author}. Your favorite genre is ${info.genre}</p>`
+    `<div id ="section"><h1 id="yourname">Hello ${info.name}</h1> <p id ="yourinfo">Your favorite book is ${info.book}. Your favorite author is ${info.author}. Your favorite genre is ${info.genre}</p><button type="button" id="erase${count}">Remove</button></div>`
   );
   DOMSelectors.name.value = "";
   DOMSelectors.book.value = "";
   DOMSelectors.author.value = "";
   DOMSelectors.genre.value = "";
-  DOMSelectors.erase.addEventListener("click", function () {
+  DOMSelectors[`erase${count}`] = document.getElementById(`erase${count}`);
+  DOMSelectors[`erase${count}`].addEventListener("click", function () {
     document.getElementById("yourname").remove();
     document.getElementById("yourinfo").remove();
+    document.getElementById(`erase${count}`).remove();
   });
 });
+
 // DOMSelectors.erase.addEventListener("click", function () {
 //   document.getElementById("yourname").remove();
 //   document.getElementById("yourinfo").remove();
