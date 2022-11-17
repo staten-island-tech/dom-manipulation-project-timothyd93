7d4890aec5042baab48b2ea1ee8e452f97869857
timothyd93 <113132240+timothyd93@users.noreply.github.com>
2022-11-16 23:21:01 -0500

    Update index.js

--- index.js
+++ index.js
@@ -7,27 +7,29 @@ const DOMSelectors = {
   author: document.querySelector(`#author`),
   genre: document.querySelector(`#genre`),
 };
-const user = {
-  name: DOMSelectors.name.value,
-  book: DOMSelectors.book.value,
-  author: DOMSelectors.author.value,
-  genre: DOMSelectors.genre.value,
-};
+
 DOMSelectors.result.addEventListener("click", function () {
-  let name = DOMSelectors.name.value;
-  let book = DOMSelectors.book.value;
-  let author = DOMSelectors.author.value;
-  let genre = DOMSelectors.genre.value;
+  const info = {
+    name: DOMSelectors.name.value,
+    book: DOMSelectors.book.value,
+    author: DOMSelectors.author.value,
+    genre: DOMSelectors.genre.value,
+  };
+
   DOMSelectors.box.insertAdjacentHTML(
     "beforeend",
-    `<h1 id="yourname">Hello ${name}</h1> <p id ="yourinfo">Your favorite book is ${book}. Your favorite author is ${author}. Your favorite genre is ${genre}</p>`
+    `<h1 id="yourname">Hello ${info.name}</h1> <p id ="yourinfo">Your favorite book is ${info.book}. Your favorite author is ${info.author}. Your favorite genre is ${info.genre}</p>`
   );
   DOMSelectors.name.value = "";
   DOMSelectors.book.value = "";
   DOMSelectors.author.value = "";
   DOMSelectors.genre.value = "";
+  DOMSelectors.erase.addEventListener("click", function () {
+    document.getElementById("yourname").remove();
+    document.getElementById("yourinfo").remove();
+  });
 });
-DOMSelectors.erase.addEventListener("click", function () {
-  document.getElementById("yourname").remove();
-  document.getElementById("yourinfo").remove();
-});
+// DOMSelectors.erase.addEventListener("click", function () {
+//   document.getElementById("yourname").remove();
+//   document.getElementById("yourinfo").remove();
+// });
